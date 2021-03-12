3a6009c86f75fc53076f232a0a5fbdaad4a50d9f
BiscuiTech <jhuet@biscui.tech>
2021-03-11 22:27:35 -0500

    wip

---
+++ src/routes/api/profile.ts
@@ -0,0 +1,12 @@
+import type { Request, Response } from "express";
+import supabase from "../../lib/supabase";
+
+export async function post(req: Request, res: Response) {
+  const { apiKey } = req.body;
+  const { data: user, error } = await supabase.auth.api.updateUser(
+    req.cookies['supaToken'],
+    {data:{"api_key": apiKey}});
+    console.log(user);
+
+  return res.end()
+}
--- src/routes/profile.svelte
+++ src/routes/profile.svelte
@@ -1,17 +1,42 @@
-<script context="module">
+<script context="module" lang="ts">
+import PageLayout from '../components/PageLayout.svelte';
   export async function preload(page, session) {
     let { userToken } = session;
     if (!userToken) return this.redirect(302, '/login');
     return userToken;
+  };
+</script>
+
+<script lang="ts">
+  let apiKey: string;
+  const handleSubmit = async () => {
+    // console.log(user, error)
+    // console.log(supabase.auth.session())
+    const authAPI = await fetch('/api/profile', {
+      method: 'POST',
+      credentials: 'same-origin',
+      body: JSON.stringify({
+        apiKey: apiKey,
+        // userId: userId
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const { success } = await authAPI.json();
+    if (success) {
+      // window.location.href = '/';
+    }
   }
 </script>
 
-<script>
-  import PageLayout from '../components/PageLayout.svelte';
-</script>
-
 <PageLayout title="Profile">
-  <form class="space-y-8 divide-y divide-gray-200">
+  <form class="space-y-8 divide-y divide-gray-200"
+    on:submit|preventDefault={(e) => {
+      // console.log(supabase.auth.session())
+      handleSubmit()
+    }}
+  >
     <div class="space-y-8 divide-y divide-gray-200">
       <div>
         <div class="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
@@ -22,9 +47,10 @@
             <div class="mt-1 flex rounded-md shadow-sm">
               <input
                 type="text"
-                name="username"
+                name="apiKey"
                 id="apiKey"
                 class="flex-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full min-w-0 rounded-none rounded-r-md sm:text-sm border-gray-300"
+                bind:value={apiKey}
               />
             </div>
           </div>
@@ -42,6 +68,7 @@
           <button
             type="submit"
             class="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            on:click={handleSubmit}
           >
             Save
           </button>
